3f92224353eb9fe75b89e1a662af9c2fd9638933
KailasRazorpay <kailas.sheregar@razorpay.com>
2019-08-20 19:24:44 +0530

    Downloader refactoring

--- download.go
+++ download.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/rs/xid"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 //Creates filepath, calls DownloadFile on the filepath and url, and updates download information
@@ -48,3 +50,79 @@ func DownloadFile(filepath string, url string) error {
 	_, err = io.Copy(out, resp.Body)
 	return err
 }
+
+func serialDownload(w http.ResponseWriter, r *http.Request, payload Payload){
+	status := "PENDING"
+	downloadId := xid.New().String()
+	files := make(map[string]string)
+	startTime := time.Now()
+	for _,url := range payload.Urls{
+		downloadSingleFile(url, downloadId, &status, files)
+	}
+	if status == "PENDING" {
+		status = "SUCCESSFUL"
+	}
+	endTime := time.Now()
+	DownloadsInfo[downloadId] = DownloadInfo{
+		Id :            downloadId,
+		Start_time :    startTime,
+		End_time :      endTime,
+		Status :        status,
+		Download_type : payload.Type,
+		Files :         files,
+	}
+	responseid,_ := json.Marshal(Response{Id: downloadId})
+	w.Header().Set("Content-Type","application/json")
+	w.Write(responseid)
+}
+
+func concurrentDownload(w http.ResponseWriter, r *http.Request, payload Payload){
+	status := "PENDING"
+	downloadId := xid.New().String()
+	files := make(map[string]string)
+	responseid,_ := json.Marshal(Response{Id: downloadId})
+	w.Header().Set("Content-Type","application/json")
+	w.Write(responseid)
+	var ch = make(chan string)
+	for i := 0; i < numberOfLinks; i++ {
+		go func() {
+			for {
+				url, ok := <-ch
+				if !ok {
+					return
+				}
+				downloadSingleFile(url, downloadId, &status, files)
+			}
+		}()
+	}
+	startTime := time.Now()
+	endTime := time.Now()
+	DownloadsInfo[downloadId] = DownloadInfo{
+		Id :            downloadId,
+		Start_time :    startTime,
+		End_time :      endTime,
+		Status :        status,
+		Download_type : payload.Type,
+		Files :         files,
+	}
+	go func() {
+		for _, url := range payload.Urls {
+			ch <- url
+		}
+		close(ch)
+		if status == "PENDING" {
+			status = "SUCCESSFUL"
+		}
+		endTime = time.Now()
+		DownloadsInfo[downloadId] = DownloadInfo{
+			Id :            downloadId,
+			Start_time :    startTime,
+			End_time :      endTime,
+			Status :        status,
+			Download_type : payload.Type,
+			Files :         files,
+		}
+		return
+	}()
+}
+
