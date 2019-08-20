154bf5e1ae6a620aa21ff500849694c5ab3af71e
KailasRazorpay <kailas.sheregar@razorpay.com>
2019-08-20 17:40:15 +0530

    Download function refactoring

--- download.go
+++ download.go
@@ -27,25 +27,24 @@ func downloadSingleFile(url string, downloadId string, status *string, files map
 	}
 }
 
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
+//DownloadFile will download a url to a local file
 func DownloadFile(filepath string, url string) error {
 
-	// Get the data
+	//Get the data
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	// Create the file
+	//Create the file
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
-	// Write the body to file
+	//Write the body to file
 	_, err = io.Copy(out, resp.Body)
 	return err
 }
