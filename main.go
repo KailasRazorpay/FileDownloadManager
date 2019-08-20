fba332c4896bbcfb5389761cfeb63ec84b52b814
KailasRazorpay <kailas.sheregar@razorpay.com>
2019-08-20 15:09:43 +0530

    Some more refactoring

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-)
-
 func main(){
-	h := http.NewServeMux()
-	h.Handle("/health", HealthHandler{})
-	h.Handle("/downloads/", StatusHandler{})
-	h.Handle("/downloads", DownloadHandler{})
-	h.HandleFunc("/files", func(w http.ResponseWriter, r *http.Request){
-		responseBody,_ := json.Marshal(DownloadsInfo)
-		w.Header().Set("Content-Type","application/json")
-		w.Write(responseBody)
-	})
-	err := http.ListenAndServe(":8002", h)
-	log.Fatal(err)
+	routes()
 }
